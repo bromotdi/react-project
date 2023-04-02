bbd8b68a348c8c27f2e30322459f5c91d01f9d39
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2023-04-02 16:10:46 +0300

    feat: delete src directory

--- src/components/Footer/Footer.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Footer } from './Footer';
-
-describe('Footer', () => {
-  it('render link elements', () => {
-    render(<Footer />);
-    expect(screen.getAllByRole('link')).toHaveLength(2);
-  });
-});
--- src/pages/HomePage/HomePage.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { HomePage } from './HomePage';
-
-describe('HomePage', () => {
-  it('render HomePage elements', () => {
-    render(<HomePage />);
-    expect(screen.getByText(/Persons/i)).toBeInTheDocument();
-  });
-});
